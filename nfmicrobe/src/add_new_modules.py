41a9a80f8691b24a4546fb10dcf07f2bf6ce94d9
Carson J. Miller <carsonjmiller@outlook.com>
2024-10-07 15:07:25 -0700

    Update nfmicrobe python scripts

--- nfmicrobe/src/add_new_modules.py
+++ nfmicrobe/src/add_new_modules.py
@@ -3,7 +3,6 @@
 import argparse
 import fileinput
 import json
-import os
 import sys
 
 
@@ -19,54 +18,60 @@ def parse_args(args=None):
         help="Path to the directory containing nf-microbe/modules repository.",
     )
     parser.add_argument(
-        "-m", "--module", help="Name of module to add",
+        "-m",
+        "--module",
+        help="Name of module to add",
     )
     return parser.parse_args(args)
 
+
 # add module to modules.json
-def update_modules_json(module_name, modules_json='modules.json'):
+def update_modules_json(module_name, modules_json="modules.json"):
     # load json file
     with open(modules_json) as file:
         modules = json.load(file)
     # add module to json
-    modules['repos']['https://github.com/nf-core/modules.git']['modules']['nf-core'][module_name] = modules['repos']['https://github.com/nf-core/modules.git']['modules']['nf-core']['coverm/contig']
+    modules["repos"]["https://github.com/nf-core/modules.git"]["modules"]["nf-core"][module_name] = modules["repos"][
+        "https://github.com/nf-core/modules.git"
+    ]["modules"]["nf-core"]["coverm/contig"]
     # sort json dict
     sorted_modules = modules.copy()
-    sorted_modules['repos']['https://github.com/nf-core/modules.git']['modules']['nf-core'] = dict(sorted(modules['repos']['https://github.com/nf-core/modules.git']['modules']['nf-core'].items()))
+    sorted_modules["repos"]["https://github.com/nf-core/modules.git"]["modules"]["nf-core"] = dict(
+        sorted(modules["repos"]["https://github.com/nf-core/modules.git"]["modules"]["nf-core"].items())
+    )
     # write json file
-    with open(modules_json, 'w') as file:
+    with open(modules_json, "w") as file:
         json.dump(sorted_modules, file, indent=4)
 
+
 # add module name to .nf-core.yml to ignore updates
-def add_module_to_nfcore_yml(module_name, nfcore_yml='.nf-core.yml'):
+def add_module_to_nfcore_yml(module_name, nfcore_yml=".nf-core.yml"):
     with fileinput.FileInput(nfcore_yml, inplace=True) as file:
         update = False
         repo = False
         nfcore = False
         for line in file:
-            if 'update:' in line:
+            if "update:" in line:
                 update = True
-            if 'https://github.com/nf-core/modules.git:' in line:
+            if "https://github.com/nf-core/modules.git:" in line:
                 repo = True
-            if 'nf-core:' in line:
+            if "nf-core:" in line:
                 nfcore = True
             if update and repo and nfcore:
-                print(line.replace('nf-core:\n', 'nf-core:\n      ' + module_name + ": False\n"), end='')
+                print(line.replace("nf-core:\n", "nf-core:\n      " + module_name + ": False\n"), end="")
                 nfcore = False
             else:
-                print(line, end='')
+                print(line, end="")
 
 
 # main function
 def main(args=None):
     args = parse_args(args)
 
-    update_modules_json(args.module, args.directory + '/modules.json')
+    update_modules_json(args.module, args.directory + "/modules.json")
 
-    add_module_to_nfcore_yml(
-        args.module,
-        args.directory + '/.nf-core.yml'
-    )
+    add_module_to_nfcore_yml(args.module, args.directory + "/.nf-core.yml")
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- nfmicrobe/src/add_nfcore_modules.py
+++ nfmicrobe/src/add_nfcore_modules.py
@@ -18,38 +18,42 @@ def parse_args(args=None):
         help="Path to the directory containing nf-microbe/modules repository.",
     )
     parser.add_argument(
-        "-m", "--module", help="Name of module to download",
+        "-m",
+        "--module",
+        help="Name of module to download",
     )
     return parser.parse_args(args)
 
+
 # convert from modules repo to pipeline repo
-def pipeline_nfcore_yml(nfcore_yml='.nf-core.yml'):
+def pipeline_nfcore_yml(nfcore_yml=".nf-core.yml"):
     with fileinput.FileInput(nfcore_yml, inplace=True) as file:
         for line in file:
-            print(line.replace('repository_type: modules', 'repository_type: pipeline'), end='')
+            print(line.replace("repository_type: modules", "repository_type: pipeline"), end="")
+
 
 # convert from pipeline repo to modules repo
-def modules_nfcore_yml(nfcore_yml='.nf-core.yml'):
+def modules_nfcore_yml(nfcore_yml=".nf-core.yml"):
     with fileinput.FileInput(nfcore_yml, inplace=True) as file:
         for line in file:
-            print(line.replace('repository_type: pipeline', 'repository_type: modules'), end='')
+            print(line.replace("repository_type: pipeline", "repository_type: modules"), end="")
+
 
 # install nf-core modules
 def install_nfcore_module(directory, module_name):
-    os.system('nf-core modules install ' + '--dir ' + directory + " " + module_name)
+    os.system("nf-core modules install " + "--dir " + directory + " " + module_name)
+
 
 # main function
 def main(args=None):
     args = parse_args(args)
 
-    pipeline_nfcore_yml(args.directory + '/.nf-core.yml')
+    pipeline_nfcore_yml(args.directory + "/.nf-core.yml")
 
-    install_nfcore_module(
-        args.directory,
-        args.module
-    )
+    install_nfcore_module(args.directory, args.module)
 
-    modules_nfcore_yml(args.directory + '/.nf-core.yml')
+    modules_nfcore_yml(args.directory + "/.nf-core.yml")
+
 
 if __name__ == "__main__":
     sys.exit(main())
