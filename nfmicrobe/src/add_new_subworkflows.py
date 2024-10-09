de671487a4fe1854106d5213d451764c5f4ddcbe
Carson J. Miller <carsonjmiller@outlook.com>
2024-10-09 13:46:33 -0700

    Added fasta_checkv_tsv

---
+++ nfmicrobe/src/add_new_subworkflows.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import argparse
+import fileinput
+import json
+import sys
+
+
+def parse_args(args=None):
+    description = "Add a new subworkflow to nf-microbe"
+    epilog = "Example usage: python add_new_subworkflow.py --subworkflow fastqc"
+
+    parser = argparse.ArgumentParser(description=description, epilog=epilog)
+    parser.add_argument(
+        "-d",
+        "--directory",
+        default=".",
+        help="Path to the directory containing nf-microbe/modules repository.",
+    )
+    parser.add_argument(
+        "-s",
+        "--subworkflow",
+        help="Name of subworkflow to add",
+    )
+    return parser.parse_args(args)
+
+
+# add module to modules.json
+def update_modules_json(subworkflow_name, modules_json="modules.json"):
+    # load json file
+    with open(modules_json) as file:
+        subworkflows = json.load(file)
+    # add module to json
+    subworkflows["repos"]["https://github.com/nf-core/modules.git"]["subworkflows"]["nf-core"][subworkflow_name] = subworkflows["repos"][
+        "https://github.com/nf-core/modules.git"
+    ]["subworkflows"]["nf-core"]["utils_nextflow_pipeline"]
+    # sort json dict
+    sorted_subworkflows = subworkflows.copy()
+    sorted_subworkflows["repos"]["https://github.com/nf-core/modules.git"]["subworkflows"]["nf-core"] = dict(
+        sorted(subworkflows["repos"]["https://github.com/nf-core/modules.git"]["subworkflows"]["nf-core"].items())
+    )
+    # write json file
+    with open(modules_json, "w") as file:
+        json.dump(sorted_subworkflows, file, indent=4)
+
+
+# add module name to .nf-core.yml to ignore updates
+def add_subworkflow_to_nfcore_yml(subworkflow_name, nfcore_yml=".nf-core.yml"):
+    with fileinput.FileInput(nfcore_yml, inplace=True) as file:
+        update = False
+        repo = False
+        nfcore = False
+        for line in file:
+            if "update:" in line:
+                update = True
+            if "https://github.com/nf-core/modules.git:" in line:
+                repo = True
+            if "nf-core:" in line:
+                nfcore = True
+            if update and repo and nfcore:
+                print(line.replace("nf-core:\n", "nf-core:\n      " + subworkflow_name + ": False\n"), end="")
+                nfcore = False
+            else:
+                print(line, end="")
+
+
+# main function
+def main(args=None):
+    args = parse_args(args)
+
+    update_modules_json(args.subworkflow, args.directory + "/modules.json")
+
+    add_subworkflow_to_nfcore_yml(args.subworkflow, args.directory + "/.nf-core.yml")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ nfmicrobe/tests/test_add_new_subworkflows.py
@@ -0,0 +1,39 @@
+import filecmp
+import os
+import shutil
+
+import src.add_new_subworkflows as add_new_subworkflows
+
+
+# test changing from modules to pipeline repo
+def test_update_modules_json():
+    shutil.copy("nfmicrobe/test_data/modules.json.base", "modules.json.test")
+    add_new_subworkflows.update_modules_json(subworkflow_name="b_test", modules_json="modules.json.test")
+    if filecmp.cmp("modules.json.test", "nfmicrobe/test_data/modules_test.json"):
+        assert True
+    os.remove("modules.json.test")
+
+
+# test adding module to nf-core.yml
+def test_add_subworkflow_to_nfcore_yml():
+    shutil.copy(".nf-core.yml", ".nf-core.yml.test")
+    add_new_subworkflows.add_subworkflow_to_nfcore_yml(subworkflow_name="b_test", nfcore_yml=".nf-core.yml.test")
+    with open(".nf-core.yml.test") as f:
+        content = f.read()
+    assert "nf-core:\n      b_test: False" in content
+    os.remove('.nf-core.yml.test')
+
+
+# test main function
+def test_main():
+    shutil.copy(".nf-core.yml", "nfmicrobe/test_data/.nf-core.yml")
+    shutil.copy("nfmicrobe/test_data/modules.json.base", "nfmicrobe/test_data/modules.json")
+    add_new_subworkflows.main(["-d", "nfmicrobe/test_data", "-s", "b_test"])
+    with open("nfmicrobe/test_data/.nf-core.yml") as f:
+        content = f.read()
+    assert "nf-core:\n      b_test: False" in content
+    if filecmp.cmp("nfmicrobe/test_data/modules.json", "nfmicrobe/test_data/modules_test.json"):
+        print
+        assert True
+    os.remove("nfmicrobe/test_data/.nf-core.yml")
+    os.remove("nfmicrobe/test_data/modules.json")
